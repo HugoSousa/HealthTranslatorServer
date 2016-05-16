e69c35effdcd5ca8a0e26c311823e701f2dd6325
hugo_sousa <77hugosousa@gmail.com>
2016-05-16 14:46:50 +0100

    Remove unused semantic type and unused supportedLanguages

--- src/java/ht/process/Processor.java
+++ src/java/ht/process/Processor.java
@@ -64,7 +64,7 @@ public class Processor {
     ServletContext servletContext;
 
     //private final ConcurrentHashMap<String, String> stopwordsEN = new ConcurrentHashMap<>();
-    private final HashSet<String> defaultSemanticTypes = new HashSet<>(Arrays.asList("T005", "T007", "T019", "T020", "T023", "T029", "T030", "T033", "T037", "T046", "T047", "T048", "T059", "T060", "T061", "T109", "T116", "T121", "T125", "T126", "T127", "T129", "T130", "T131", "T184", "T190", "T194", "T195", "T200", "T204"));
+    private final HashSet<String> defaultSemanticTypes = new HashSet<>(Arrays.asList("T005", "T007", "T019", "T020", "T023", "T029", "T030", "T037", "T046", "T047", "T048", "T059", "T060", "T061", "T109", "T116", "T121", "T125", "T126", "T127", "T129", "T130", "T131", "T184", "T190", "T194", "T195", "T200", "T204"));
 
     //private Matcher punctuationMatcher;
     //private Matcher numberMatcher;
@@ -112,10 +112,6 @@ public class Processor {
     @Produces("application/json")
     @Consumes("application/json")
     public ProcessorResult2 process(ProcessorParams param) {
-
-        System.out.println(param);
-        if(param.supportedLanguages == null || param.supportedLanguages.isEmpty())
-            param.setDefaultSupportedLanguages();
 
         //System.out.println("Starting Processing");
         SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss.SSS");
--- src/java/ht/process/ProcessorParams.java
+++ src/java/ht/process/ProcessorParams.java
@@ -22,17 +22,9 @@ public class ProcessorParams {
     @XmlElement public String styFilter;
     @XmlElement public Boolean recognizeOnlyCHV;
     @XmlElement public Boolean recognizeWithoutDefinition;
-    @XmlElement public ArrayList<String> supportedLanguages;
     @XmlElement public String contentLanguage;
     @XmlElement public HashSet<String> semanticTypes = new HashSet<>(); //Arrays.asList("T005", "T007", "T023", "T029", "T030", "T034", "T037", "T040", "T046", "T047", "T048", "T059", "T060", "T061", "T116", "T121", "T125", "T126", "T127", "T129", "T130", "T131", "T184", "T192", "T195", "T200"));
     
     public ProcessorParams(){
     }
-    
-    public void setDefaultSupportedLanguages(){
-        supportedLanguages = new ArrayList<String>();
-        supportedLanguages.add("en");
-        supportedLanguages.add("pt");
-    }
-
 }
